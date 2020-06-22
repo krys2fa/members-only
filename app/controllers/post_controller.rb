7dee32528d4a623d65040b0aef580a37b4664246
Christopher Amanor <kriskit7@gmail.com>
2020-06-22 13:56:38 +0000

    Add posts controller and post model

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,10 @@
+class PostController < ApplicationController
+  def create
+  end
+
+  def new
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'post/create'
+  get 'post/new'
+  get 'post/index'
   devise_for :users
   root to: "posts#index"
 end
